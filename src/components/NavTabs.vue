f3c2335a1e7912652b3ef60dc5618009f97f06f1
wendy hsiao <wendy19940331@gmail.com>
2020-05-14 16:42:10 +0800

    【重構】NavTabs元件

--- src/components/NavTabs.vue
+++ src/components/NavTabs.vue
@@ -1,36 +1,48 @@
 <template>
   <ul class="nav nav-tabs mb-4">
-    <li class="nav-item">
+    <li 
+      v-for="tab in tabs"
+      :key="tab.id"
+      class="nav-item"
+    >
       <router-link
-        to="/restaurants"
+        :to="tab.path"
         class="nav-link"
       >
-        首頁
-      </router-link>
-    </li>
-    <li class="nav-item">
-      <router-link
-        to="/restaurants/feeds"
-        class="nav-link"
-      >
-        最新動態
-      </router-link>
-    </li>
-    <li class="nav-item">
-      <router-link
-        to="/restaurants/top"
-        class="nav-link"
-      >
-        TOP 10 人氣餐廳
-      </router-link>
-    </li>
-    <li class="nav-item">
-      <router-link
-        to="/users/top"
-        class="nav-link"
-      >
-        美食達人
+        {{tab.title}}
       </router-link>
     </li>
   </ul>
 </template>
+
+<script>
+import { v4 as uuidv4 } from 'uuid'
+export default {
+  data () {
+    return {
+      tabs: [
+        {
+          id: uuidv4(),
+          title: '首頁',
+          path: '/restaurants'
+        },
+        {
+          id: uuidv4(),
+          title: '最新動態',
+          path: '/restaurants/feeds'
+        },
+        {
+          id: uuidv4(),
+          title: 'TOP 10 人氣餐廳',
+          path: '/restaurants/top'
+        },
+        {
+          id: uuidv4(),
+          title: '美食達人',
+          path: '/users/top'
+        }
+      ]
+    }
+  }
+}
+</script>
