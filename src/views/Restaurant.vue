9480927086c9a75f55262f8d2e6ab31cc4ce9fb6
wendy hsiao <wendy19940331@gmail.com>
2020-05-21 15:17:29 +0800

    【加入】RestaurantDetail 元件，傳入餐廳資料

--- src/views/Restaurant.vue
+++ src/views/Restaurant.vue
@@ -2,6 +2,7 @@
   <div class="container py-5">
     <h1>餐廳描述頁</h1>
     <!-- 餐廳資訊頁 RestaurantDetail -->
+    <RestaurantDetail :initial-restaurant="restaurant" />
     <hr />
     <!-- 餐廳評論 RestaurantComments -->
     <!-- 新增評論 CreateComment -->
@@ -9,6 +10,8 @@
 </template>
 
 <script>
+import RestaurantDetail from '../components/RestaurantDetail.vue'
+
 const dummyDate = {
   restaurant: {
     id: 5,
@@ -348,6 +351,9 @@ const dummyDate = {
 };
 
 export default {
+  components: {
+    RestaurantDetail
+  },
   data() {
     return {
       restaurant: {
