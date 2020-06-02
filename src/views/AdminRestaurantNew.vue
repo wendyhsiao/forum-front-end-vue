749a24e7ff3169949a0de230259a464f097c5389
wendy.hsiao <wendy19940331@gmail.com>
2020-06-03 01:05:43 +0800

    【加入】AdminRestaurantNew 頁面，建立路由

---
+++ src/views/AdminRestaurantNew.vue
@@ -0,0 +1,14 @@
+<template>
+ <div class="container py-5">
+    <!-- 餐廳表單 AdminRestaurantForm -->
+  </div>
+</template>
+
+<script>
+import AdminRestaurantForm from '../components/AdminRestaurantForm.vue'
+export default {
+  components: {
+    AdminRestaurantForm
+  },
+}
+</script>
