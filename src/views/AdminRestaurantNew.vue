309c29b76bfeff98a99de522dab7f7900dd59dda
wendy.hsiao <wendy19940331@gmail.com>
2020-06-03 01:07:14 +0800

    【加入】AdminRestaurantForm 元件，後台新增餐廳功能

--- src/views/AdminRestaurantNew.vue
+++ src/views/AdminRestaurantNew.vue
@@ -1,6 +1,7 @@
 <template>
  <div class="container py-5">
     <!-- 餐廳表單 AdminRestaurantForm -->
+    <AdminRestaurantForm @after-submit="handleAfterSubmit" />
   </div>
 </template>
 
@@ -10,5 +11,13 @@ export default {
   components: {
     AdminRestaurantForm
   },
+  methods: {
+    handleAfterSubmit(formData) {
+      // 透過 API 將表單資料送到伺服器
+      for (let [name, value] of formData.entries()) {
+        console.log(name + ': ' + value)
+      }
+    }
+  }
 }
 </script>
