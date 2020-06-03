671c4b13f94f2379838b71cdd73c09a6c85fa406
wendy.hsiao <wendy19940331@gmail.com>
2020-06-03 16:29:24 +0800

    【修改】後台對應連結

--- src/components/AdminNav.vue
+++ src/components/AdminNav.vue
@@ -4,7 +4,9 @@
     <router-link 
       to="/admin/restaurants"
     >Restaurants</router-link> |
-    <a href="#">Categories</a> |
+    <router-link
+      to="/admin/categories">Categories
+    </router-link> |
     <a href="#">Users</a>
   </div>
 </template>
