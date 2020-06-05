8297e17ca6671eae4c96cb67d667fb77256f1a5a
wendy.hsiao <wendy19940331@gmail.com>
2020-06-05 12:07:55 +0800

    【加入】SignIn頁，防止重複點擊功能

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -39,6 +39,7 @@
       <button
         class="btn btn-lg btn-primary btn-block mb-3"
         type="submit"
+        :disabled="isProcessing"
       >
         Submit
       </button>
@@ -66,7 +67,8 @@ export default {
   data () {
     return {
       email: '',
-      password: ''
+      password: '',
+      isProcessing: false
     }
   },
   methods: {
@@ -80,6 +82,8 @@ export default {
         return
       }
       
+      this.isProcessing = true
+
       authorizationAPI.signIn({
         email: this.email,
         password: this.password
@@ -103,6 +107,9 @@ export default {
           icon: 'warning',
           title: '請確認您輸入了正確的帳號密碼',
         })
+
+        // 因為登入失敗，所以要把按鈕狀態還原
+        this.isProcessing = false
         console.log('error', error)
       })
     }
