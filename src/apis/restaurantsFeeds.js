a75da1ff6285441f6e5c6ca2772cb1572909e141
wendy.hsiao <wendy19940331@gmail.com>
2020-06-30 10:58:51 +0800

    【重構】axios API Helper

--- src/apis/restaurantsFeeds.js
+++ src/apis/restaurantsFeeds.js
@@ -1,10 +1,7 @@
 import { apiHelper } from '../utils/helpers.js'
-const getToken = () => window.localStorage.getItem('token')
 
 export default {
   getRestaurantsFeeds () {
-    return apiHelper.get('/restaurants/feeds', {
-      headers: { Authorization: `Bearer ${getToken()}` }
-    })
+    return apiHelper.get('/restaurants/feeds')
   }
 }
